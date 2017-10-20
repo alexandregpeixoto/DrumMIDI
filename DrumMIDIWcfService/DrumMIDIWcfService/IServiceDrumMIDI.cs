32f655029c637bade21beb7e02f5a53e331a80e0
Alexandre Gervasi Peixoto <alexandregpeixoto@gmail.com>
2017-10-20 20:43:38 -0200

    Desenvolvimento do Web Service
    Talvez seja necessário repensar a base de dados...

--- DrumMIDIWcfService/DrumMIDIWcfService/IServiceDrumMIDI.cs
+++ DrumMIDIWcfService/DrumMIDIWcfService/IServiceDrumMIDI.cs
@@ -20,5 +20,8 @@ namespace DrumMIDIWcfService
 
         [OperationContract]
         Boolean UserCreate(String _UserName, String _Password);
+
+        [OperationContract]
+        Boolean PresetCreate(string _PresetName, Int32 _idPart1, Int32 _idPart2, Int32 _idPart3, Int32 _idPart4, Int32 _idPart5, Int32 _idUser);
     }
 }
--- DrumMIDIWcfService/DrumMIDIWcfService/ServiceDrumMIDI.svc.cs
+++ DrumMIDIWcfService/DrumMIDIWcfService/ServiceDrumMIDI.svc.cs
@@ -24,10 +24,19 @@ namespace DrumMIDIWcfService
                 newSqlConnection.Open();
                 using (SqlCommand newCommand = new SqlCommand("INSERT INTO tbUser([Name], [Password]) VALUES ('" + _UserName + "','" + _Password + "')", newSqlConnection))
                 {
-                    try { if (newCommand.ExecuteNonQuery() != 0) { fResult = true; }; }
+                    try
+                    {
+                        if (newCommand.ExecuteNonQuery() != 0)
+                        {
+                            fResult = true;
+                        }
+                    }
 
                     //If there's some error, just ignore the command and return false...
-                    catch { }
+                    catch
+                    {
+
+                    }
                 }
             }
             webServiceDB = null;
@@ -44,9 +53,17 @@ namespace DrumMIDIWcfService
                 using (SqlCommand newCommand = new SqlCommand("SELECT [Name] FROM tbUser", newSqlConnection))
                 using (SqlDataReader newReader = newCommand.ExecuteReader())
                 {
-                    while (newReader.Read())
+                    try
                     {
-                        fResult.Add(newReader.GetString(0));
+                        while (newReader.Read())
+                        {
+                            fResult.Add(newReader.GetString(0));
+                        }
+                    }
+
+                    catch
+                    {
+
                     }
                 }
             }
@@ -64,9 +81,45 @@ namespace DrumMIDIWcfService
                 using (SqlCommand newCommand = new SqlCommand("SELECT [Password] FROM tbUser WHERE [UserName] = " + _UserName, newSqlConnection))
                 using (SqlDataReader newReader = newCommand.ExecuteReader())
                 {
-                    while (newReader.Read())
+                    try
                     {
-                        if (newReader.GetString(0) == _Password) { fResult = true; };
+                        while (newReader.Read())
+                        {
+                            if (newReader.GetString(0) == _Password) { fResult = true; };
+                        }
+                    }
+
+                    catch
+                    {
+
+                    }
+                }
+            }
+            webServiceDB = null;
+            return fResult;
+        }
+
+        public bool PresetCreate(string _PresetName, Int32 _idPart1, Int32 _idPart2, Int32 _idPart3, Int32 _idPart4, Int32 _idPart5, Int32 _idUser)
+        {
+            bool fResult = false;
+            DbConnection webServiceDB = new DbConnection();
+            using (SqlConnection newSqlConnection = new SqlConnection(webServiceDB.ConnectionString))
+            {
+                newSqlConnection.Open();
+                using (SqlCommand newCommand = new SqlCommand("INSERT INTO tbPreset([presetName], [idPart1], [idPart2], [idPart3], [idPart4], [idPart5], [idUser]) VALUES ('" + _PresetName + "','" + _idPart1 + "','" + _idPart2 + "','" + _idPart3 + "','" + _idPart4 + "','" + _idPart5 + "','" + _idUser + "')", newSqlConnection))
+                {
+                    try
+                    {
+                        if (newCommand.ExecuteNonQuery() != 0)
+                        {
+                            fResult = true;
+                        }
+                    }
+
+                    //If there's some error, just ignore the command and return false...
+                    catch
+                    {
+
                     }
                 }
             }
